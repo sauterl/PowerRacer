9a15978fde1e1d10b5987ba329dd04d892d22078
Florian Spiess <florian.spiess@unibas.ch>
2021-01-11 09:12:13 +0100

    Disabled empty lobby autoremove and implemented server lobby remove command.

--- src/main/java/server/lobby/UpdateManager.java
+++ src/main/java/server/lobby/UpdateManager.java
@@ -20,22 +20,20 @@ class UpdateManager extends Thread {
 		ServerGUI.addToConsole("UpdateManager started.");
 		try {
 			while (!terminate) {
-				for (int i = 0; i < LobbyManager.lobbylist.size(); i++) {
-					if (LobbyManager.lobbylist.get(i).getLobbylist().size() == 0) {
-						LobbyLogic.sendLobbyDeletion(LobbyManager.lobbylist
-								.get(i).getLobbyID());
-						ServerGUI.addToConsole("Lobby with ID: "
-								+ LobbyManager.lobbylist.get(i).getLobbyID()
-								+ " destroyed.");
-						LobbyManager.lobbylist.remove(i);
-					}
-				}
+//				for (int i = 0; i < LobbyManager.lobbylist.size(); i++) {
+//					if (LobbyManager.lobbylist.get(i).getLobbylist().size() == 0) {
+//						LobbyLogic.sendLobbyDeletion(LobbyManager.lobbylist.get(i).getLobbyID());
+//						ServerGUI.addToConsole("Lobby with ID: "
+//								+ LobbyManager.lobbylist.get(i).getLobbyID()
+//								+ " destroyed.");
+//						LobbyManager.lobbylist.remove(i);
+//					}
+//				}
 				Thread.sleep(5000);
 			}
 		} catch (Exception e) {
 			ServerGUI.addToConsole("Exception in UpdateManager:");
-			ServerGUI.addToConsole(Arrays.toString(e.getStackTrace())
-					.replaceAll(", ", "\n"));
+			ServerGUI.addToConsole(Arrays.toString(e.getStackTrace()).replaceAll(", ", "\n"));
 		}
 	}
 
